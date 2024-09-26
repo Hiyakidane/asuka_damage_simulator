7c242f5172c4abc48af11accfc3156bb09fd4180
火焼種くべる <chicken.c.c@gmail.com>
2024-09-26 21:48:18 +0900

    change directory

--- asuka_damage_simulator/src/App.tsx
+++ asuka_damage_simulator/src/App.tsx
@@ -1,23 +1,9 @@
 // React
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from "react-router-dom";
 
 import { Top } from "./features/contents/components/Top";
 
 export const App = () => {
-    {/*}
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" Component={Top} />
-            </Routes>
-        </Router>
-    );
-    {*/}
     return (
         <>
             <Top />
--- asuka_damage_simulator/src/features/contents/components/Header.tsx
+++ asuka_damage_simulator/src/features/contents/components/Header.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Header = () => {
     return (
-        <h3>不思議のダンジョン 風来のシレン外伝 女剣士アスカ見参！ダメージ計算機</h3>
+        <></>
     );
 }
